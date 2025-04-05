c3b16008400294a99c7858d49e6636c378bfc848
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2025-04-05 06:52:16 -0300

    setup swagger

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const { shallowCopy } = require('ethers/lib/utils');
+const swaggerAutogen = require('swagger-autogen');
+
+const doc = {
+    info: {
+        title: 'Challenging API',
+        description: 'Daily social challenge API application where the user can develop courage, eliminate social anxiety and lose fear. The challenges will be split into four categories: emotional, physical, intellectual and social challenges.'
+    },
+    host: 'localhost:3000',
+    schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointFiles, doc);
